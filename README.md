057f29528911d60d7f66a3ba43176d86bee4ebae
Benoit Mayeur <benoitmayeur@gmail.com>
2020-04-16 15:25:16 +0200

    Whole Project

---
+++ README.md
@@ -0,0 +1,12 @@
+Pour la formation de développeur web full-stack JS
+
+Adaptation en Vanilla JS d'un exercice d'Introduction à la programmation du Pr. Corda
+
+Exercice 10 : Colis chariot
+
+Un chariot peut transporter 3 colis mais il ne peut transporter plus de 500kg. Créer un programme qui demandera le poids de chaque colis et vérifiera si le chariot peut les transporter.
+Attention : avertissez l’utilisateur si le transport peut se faire ou non via les messages suivants : OK pour transport ou Surcharge
+Exemple : Colis 1 = 75
+Colis 2 = 145
+Colis 3 = 200
+Total = 420 : OK pour transport
---
+++ js/main.js
@@ -0,0 +1,59 @@
+let weight_parcels= [];
+let amount_parcels = 0;
+let total_weight_parcels = 0;
+let weight_current_parcel;
+let button;
+
+window.onload = function() {
+    document.getElementById('weight_parcel').value = '';
+    weight_parcels =[];
+    amount_parcels = 0;
+    total_weight_parcels = 0;
+    weight_current_parcel = 0;
+    }
+
+
+if(document.getElementById("weight_parcel") != null){
+
+    document.getElementById("submit").addEventListener('click', function (event) {
+    
+        weight_current_parcel = document.getElementById("weight_parcel").value;
+        total_weight_parcels = 0;
+
+        Number(weight_current_parcel);
+        console.log("weight_current_parcel" + weight_current_parcel);
+        if(isNaN(weight_current_parcel)){
+            document.getElementById('message_parcel').innerHTML = 'Mettez un poids sous un format numérique';
+        }
+        else{
+
+            weight_parcels.push(weight_current_parcel);
+            console.log(weight_parcels);
+            amount_parcels++;
+
+            if(amount_parcels>=3){
+                document.getElementById('message_parcel').innerHTML = "Limite du nombre de colis atteinte, impossible d'ajouter un colis en plus ";
+                button = document.querySelector('button')
+                button.disabled = true
+            }
+
+            for(let i=0; i<weight_parcels.length; i++){
+                total_weight_parcels = total_weight_parcels + Number(weight_parcels[i]);
+            }
+
+            if(total_weight_parcels>=500){
+                document.getElementById('message_parcel').innerHTML = 'Limite de poids atteinte, transport impossible';
+            }
+
+
+            document.getElementById('total_weight').innerHTML = total_weight_parcels;
+
+
+            console.log(total_weight_parcels)
+        }
+    
+    
+        event.preventDefault();
+    });
+}
+
